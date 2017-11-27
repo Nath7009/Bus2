bd49fa951e3be68e57d16a6684d7a5145b535e69
Nath7009 <cantatnathan@gmail.com>
2017-11-27 11:27:13 +0100

    Connexion

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
+char MDP[]="admin";
 
 typedef struct
 {
@@ -29,13 +30,32 @@ typedef struct
 int main()
 {
     FILE *fichier;
-    char c;
-    fichier=fopen("Ligne1.csv","r+");
-    while(!feof(fichier)){
-        c=fgetc(fichier);
-        printf("%c",c);
+    char motDePasse[20]="0";
+    int choix;
+    do
+    {
+        do
+        {
+            printf("Etes vous un simple utlisateur ou un administateur ? 1 pour utilisateur et 2 pour administrateur\n");
+            scanf("%d",&choix);
+        }
+        while(choix !=1 && choix !=2);
+        if(choix ==2)
+        {
+            printf("Entrer le  mot de passe \n");
+            scanf("%s",motDePasse);
+        }
     }
-    printf("%d",sizeof(ligne));
+    while(strcmp(MDP,motDePasse)!=0 && choix==2);
+    if(choix==1)
+    {
+        printf("Vous pouvez acceder au programme en tant que simple utilisateur");
+    }
+    else
+    {
+        printf("Vous pouvez acceder au programme en tant qu'admin");
+
+    }
     return 0;
 }
 
