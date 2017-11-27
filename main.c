0e2a784f4bf71f3d9acb48765ca4d4c73f1dea4e
Nath7009 <cantatnathan@gmail.com>
2017-11-27 11:40:57 +0100

    sommaire

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main()
 {
     FILE *fichier;
     char motDePasse[20]="0";
-    int choix;
+    int choix,choixMenu;
     do
     {
         do
@@ -49,11 +49,43 @@ int main()
     while(strcmp(MDP,motDePasse)!=0 && choix==2);
     if(choix==1)
     {
-        printf("Vous pouvez acceder au programme en tant que simple utilisateur");
+        printf("Vous pouvez acceder au programme en tant que simple utilisateur\n");
+        do
+        {
+            printf("Que voulez vous faire ?\n");
+            printf("1-Donner l'horaire de passage du prochain bus pour un arret\n") ;
+            printf("2-Donner l'horaire d'une ligne pour un arret\n");
+            printf("3-Consulter les horaires d'un arret de bus\n");
+            printf("4-Sauvegarder les horaires d'un arret d'une ligne dans un fichier\n");
+            printf("5-Quitter le programme\n");
+            scanf("%d",&choixMenu);
+            if(choixMenu>=1 && choixMenu<5)
+            {
+                switch(choixMenu)
+                {
+                case 1 :
+
+                    break;
+                case 2 :
+
+                    break;
+                case 3 :
+
+                    break;
+                case 4 :
+
+                    break;
+
+
+                }
+            }
+        }
+        while(choixMenu!=5);
+
     }
     else
     {
-        printf("Vous pouvez acceder au programme en tant qu'admin");
+        printf("Vous pouvez acceder au programme en tant qu'admin\n");
 
     }
     return 0;
