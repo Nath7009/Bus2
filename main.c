f1725270f394f1fbe4fb3b8b2f9502d30d8d5604
Nath7009 <cantatnathan@gmail.com>
2017-12-18 11:17:13 +0100

    Ajout de quelques fonctions admin

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main()
     FILE *fichier;
     char motDePasse[20],*nomArret,nomLigne[10];
     int log=0,i,j,k,reussi=0,ecartMinutes, ecartMiniMinutes=10000;
-    int numLigne;
+    int numLigne,nbHoraires=0;
     int choixMenu,choixLigne;
     heure heureActu,ecart;
     heureActu.hr=0;
@@ -267,9 +267,9 @@ int main()
             printf("2-Donner l'horaire d'une ligne pour un arret\n");
             printf("3-Consulter les horaires d'un arret de bus\n");
             printf("4-Sauvegarder les horaires d'un arret d'une ligne dans un fichier\n");
-            printf("5-Ajouter un arrêt\n");
-            printf("6-Modifier le nom d'un arrêt\n");
-            printf("7-Supprimer un arrêt\n");
+            printf("5-Ajouter un arret\n");
+            printf("6-Modifier le nom d'un arret\n");
+            printf("7-Supprimer un arret\n");
             printf("8-Creer une ligne\n");
             printf("9-Modifier une ligne\n");
             printf("10-Supprimer une ligne\n");
@@ -493,6 +493,76 @@ int main()
                 case 5:
                     //Ajouter un arret
                     printf("Entrer le numero de la ligne a laquelle vous voulez changer l'arret\n");
+                    scanf("%d",&numLigne);
+                    viderBuffer();
+                    i=0;
+                    while(i<NB_LIGNES && lignes[i].nbLigne!=numLigne) i++; //On cherche la ligne dans le tableau des lignes
+
+                    if(i<NB_LIGNES-1)  //Si il y a plusieurs lignes avec le meme numero, on propose à l'utilisateur de choisir la bonne direction
+                    {
+                        if(lignes[i+1].nbLigne==numLigne)
+                        {
+                            printf("Il y a plusieurs directions correspondant a la ligne que vous cherchez\n");
+                            do
+                            {
+                                printf("Veuillez entrer le numero de la ligne que vous cherchez\n");
+                                j=i;
+                                while(lignes[j].nbLigne==numLigne && j<NB_LIGNES)  //On parcourt les lignes
+                                {
+                                    printf("%d),%s\n",j-i,lignes[j].nomLigne); //On donne le nombre à entrer et on le fait partir de 0 avec j-i, puis 1,2,... tant qu'on a une ligne avec le bon numéro
+                                    j++;
+                                }
+                                scanf("%d",&choixLigne);
+                                viderBuffer();
+                            }
+                            while(choixLigne<0 && choixLigne>j-i-1); //Sécurité, j-i-1 car on inrémente j à la fin de la boucle
+                            lign=&lignes[choixLigne+i];//On récupère l'adresse de la bonne ligne pour chercher un arret dedans
+                        }
+                    }
+                    else if(i<NB_LIGNES)
+                    {
+                        lign=&lignes[i];
+                    }
+                    else
+                    {
+                        printf("La ligne que vous cherchez n'est pas disponible\n");
+                        break;
+                    }
+                    printf("La ligne que vous cherchez a ete trouvee\n");
+                    printf("Entrer le nom de l'arret que vous voulez ajouter\n");
+                    gets(nomArret);
+                    lign->arrets=(arret*)realloc(lign->arrets,sizeof(arret)*(lign->nbArrets+1));
+                    strcpy(lign->arrets[lign->nbArrets].nom,nomArret); //On copie le nom de l'arret dans la bonne case memoire
+                    lign->nbArrets++; //On incrémente le nombre d'arrets dans la ligne
+                    printf("Entrer le nombre d'horaires que vous souhaitez entrer\n");
+                    scanf("%d",&nbHoraires);
+                    viderBuffer();
+                    lign->arrets[lign->nbArrets-1].heures=(heure*)malloc(sizeof(heure)*nbHoraires); //On a nbArrets = nombre d'arrets +1, donc on accede à nbArrets-1
+                    lign->arrets[lign->nbArrets-1].heures[0].hr=0;
+                    lign->arrets[lign->nbArrets-1].heures[0].mn=0;
+                    for(i=0;i<nbHoraires;i++){ //On parcourt tous les horaires pour le saisir
+                        printf("Entrer l'horaire numero %d\n",i+1); //On met i+1 pour commencer a 1
+                        do{
+                        printf("Entrer l'heure\n");
+                        scanf("%d",&lign->arrets[lign->nbArrets-1].heures[i].hr);
+                        printf("Entrer la minute\n");
+                        scanf("%d",&lign->arrets[lign->nbArrets-1].heures[i].mn);
+                        if(i>0 &&
+                           lign->arrets[lign->nbArrets-1].heures[i].mn-lign->arrets[lign->nbArrets-1].heures[i-1].mn<1 //Si on écrit une horaire à moins de 1 minute d'intervalle
+                           || lign->arrets[lign->nbArrets-1].heures[i].hr-lign->arrets[lign->nbArrets-1].heures[i].hr<0){ //Si on écrit une horaire à une heure inférieure à l'heure d'avant
+                            printf("L'heure que vous avez saisi n'est pas valide\n");
+                        }
+                        }while((lign->arrets[lign->nbArrets-1].heures[i].mn < 0  //On teste si l'heure est valide
+                               || lign->arrets[lign->nbArrets-1].heures[i].mn > 60
+                               ||lign->arrets[lign->nbArrets-1].heures[i].hr < 0
+                               ||lign->arrets[lign->nbArrets-1].heures[i].hr > 24)  //Si l'heure n'est pas correcte ou si les heures sont trop proches
+                               ||(i>0
+                                &&(lign->arrets[lign->nbArrets-1].heures[i].mn-lign->arrets[lign->nbArrets-1].heures[i-1].mn<1 //Si on écrit une horaire à moins de 1 minute d'intervalle
+                                || lign->arrets[lign->nbArrets-1].heures[i].hr-lign->arrets[lign->nbArrets-1].heures[i-1].hr<0
+                                || lign->arrets[lign->nbArrets-1].heures[i].hr>lign->arrets[lign->nbArrets-1].heures[i-1].hr
+                                && lign->arrets[lign->nbArrets-1].heures[i].mn<lign->arrets[lign->nbArrets-1].heures[i-1].mn
+                                )));
+                    }
                     break;
                 case 6:
                     printf("Entrer le nom de l'arret\n");
